0d0dab23ffc2bf9b9d1bfc46adbf003b071e212b
Michael R <30969281+mrippey@users.noreply.github.com>
2022-09-17 10:04:17 +0900

    Create detect_mal_url.py

---
+++ detect_mal_url.py
@@ -0,0 +1,65 @@
+"""_detect_mal_url.py_ - Detect malicious domains using Logistic Regression"""
+
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from rich.console import Console
+from core.logs import LOG
+
+LOG.info("Starting detect_mal_url.py")
+console = Console()
+
+domain_dataset = pd.read_csv(
+        "/path/to/your/training/testing/csv_data"
+    )
+
+def detect_domains(word_list):
+    """_summary_ - Detect malicious domains using Logistic Regression"""
+
+    LOG.debug("Loading test dataset")
+    
+    data = domain_dataset["url"]
+    classifier = domain_dataset["label"]
+    LOG.debug("Splitting dataset into training and testing sets")
+    vectorizer = TfidfVectorizer()
+    x_info = vectorizer.fit_transform(data)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_info, classifier, test_size=0.2, random_state=42
+    )
+    LOG.debug("Training model")
+    model = LogisticRegression(max_iter=1000)
+    model.fit(x_train, y_train)
+    LOG.debug("Testing model")
+    score = model.score(x_test, y_test)
+    console.print(f"[*] Model accuracy: {score:.2f}", style="bold white")
+    LOG.debug("Model accuracy: %s", score)
+    print()
+ 
+    LOG.debug("Predicting malicious domains")
+    with open(
+        word_list,
+        "r",
+    ) as outfile:
+        word_list = outfile.readlines()
+    domain_matches = [line.strip() for line in word_list]
+
+    predictions = model.predict(vectorizer.transform(domain_matches))
+
+    mal_domains_file = '/mal_domains.txt'
+
+    for i in range(len(domain_matches)):  #range(len(text))
+        
+        if predictions[i] == "bad":
+            
+            console.print(
+                f"[bold white]{domain_matches[:5]}[/bold white] : [bold red]likely malicious[/bold red]"
+            )
+        else:
+            continue
+
+        with open(mal_domains_file, "a") as outfile:
+            outfile.write(domain_matches[i] + "\n")
+        console.print(f'[*] Domains identified by ML algorithm written to: {mal_domains_file}', style="bold red")
+  
+
