dc7faf9ae2cf394ec75ba11ea891d89554e2fff2
Mike R <cr8syoki@gmail.com>
2022-12-23 20:51:57 +0900

    refactor domain_lkup_summary.py

--- infratrack/domain_lkup_summary.py
+++ infratrack/domain_lkup_summary.py
@@ -1,6 +1,5 @@
 """Domain Lookup Summary"""
 import os
-import time
 from dotenv import load_dotenv
 import whois
 from httpx import get
@@ -8,8 +7,7 @@ import dns.resolver
 from rich.console import Console
 from rich import table
 from core.logs import LOG
-from riskiq_api_lookup import riskiq_ip_resolutions
-from output_table import create_table_stuff
+
 
 load_dotenv()
 console = Console()
@@ -17,7 +15,9 @@ console = Console()
 LOG.info("Starting domain_lkup_summary.py...")
 
 
+
 class DomainSummary:
+    """_summary_ - DomainSummary class"""    
     def __init__(self, target_domain: str):
         self.target_domain = target_domain
         self.vt_api = os.getenv("VT_API_KEY")
@@ -27,39 +27,29 @@ class DomainSummary:
             f"https://www.virustotal.com/api/v3/domains/{target_domain}"
         )
         self.vt_domain_commfiles = f"https://www.virustotal.com/api/v3/domains/{target_domain}/communicating_files"
-        self.vt_domain_certinfo = f"https://www.virustotal.com/api/v3/domains/{target_domain}/historical_ssl_certificates"
+        self.vt_domain_collections = f"https://www.virustotal.com/api/v3/domains/{target_domain}/collections"
+        self.pdns_resolutions = []
 
     def virustotal_api_req_domaininfo(self, target_domain):
         """_summary_ - VirusTotal API request for domain info"""
         print()
-        console.print(
-            f"[!] VirusTotal results for {target_domain}: ",
-            style="bold white",
-        )
-
         LOG.debug("VT API GET request for %s", self.vt_domain_report)
         response = get(self.vt_domain_report, headers=self.api_key_header)
         results = response.json()
         LOG.debug("Received a response: %s", results)
-        if results.get("error") and results.get("data") is None:
-            console.print(
-                "[X] Either VirusTotal returned 0 results, or there was an error in your domain pattern. Continuing...",
-                style="bold red",
-            )
 
-        else:
-            data = results["data"]
-            attrs = data["attributes"]
-            last_analysis = attrs["last_analysis_stats"]
-            mal = last_analysis["malicious"]
+        if results.get("error") and results.get("data") is None:
+            return "[X] Either VirusTotal returned 0 results, or there was an error in your domain pattern. Continuing..."
+        data = results["data"]
+        attrs = data["attributes"]
+        last_analysis = attrs["last_analysis_stats"]
+        mal = last_analysis["malicious"]
 
         if mal == 0:
-            console.print(f"[X] {target_domain} is clean", style="bold red")
-        else:
-            console.print(
-                f"[!] {target_domain} was identified as malicious by {str(mal)} vendors",
-                style="bold green",
-            )
+            return f"{target_domain} is clean"
+        return f"{target_domain} was identified as malicious by {str(mal)} vendors"
+        
+
 
     def virustotal_api_req_commfiles(self, target_domain):
         """_summary_ - VirusTotal API request for communicating files"""
@@ -70,7 +60,7 @@ class DomainSummary:
         LOG.debug("Received a response: %s", results)
         if results.get("error") and results.get("data") is None:
             console.print(
-                "[X] Either VirusTotal returned 0 results, or there was an error in your domain pattern. Continuing...",
+                "Either VirusTotal returned 0 results, or there was an error in your domain pattern. Continuing...",
                 style="bold red",
             )
 
@@ -79,22 +69,16 @@ class DomainSummary:
             file_count = metadata["count"]
 
             if file_count == 0:
-                console.print(
-                    f"[X] No communicating files with {target_domain}", style="bold red"
-                )
-
-            else:
-                console.print(
-                    f'[!] {file_count} file(s) communicating with [bold white] {target_domain}',
-                    style="bold green",
-                )
-
+               
+                return f"No communicating files with {target_domain}"
+               
+            return f"{file_count} file(s) communicating with [bold white] {target_domain}"
+    
 
     def run(self):
-        """_summary_ - Run the VirusTotal API requests"""
+        """_summary_ - Run the main code"""
         try:
-            if _ := whois.whois(self.target_domain):
-                self.api_output()
+            self.combine_data_to_table()
         except dns.resolver.NXDOMAIN as err:
             console.print(err, style="bold red")
 
@@ -102,44 +86,57 @@ class DomainSummary:
             console.print(err, style="bold red")
             return "There was an issue with the domain name. Please check your input and try again."
 
-
-    def api_output(self):
-        """_summary_ - Output VirusTotal API results"""
+    def combine_data_to_table(self):
+        """_summary_ - Combine all relevant API data and create a table for output"""        
         console.print(
-            f"[!] Displaying results for {self.target_domain}: ", style="bold white"
-        )
-        host_to_ip = dns.resolver.query(self.target_domain, "A")
-        #for ipval in host_to_ip:
-            #console.print(f"[!] IP address: {ipval.to_text()}", style="bold green")
-        domain_info = whois.whois(self.target_domain)
-        infratrack_table = table.Table(title ="Test",show_header=True, header_style="white", show_footer=False)
+            f"[!] Grabbing results for {self.target_domain}: ", style="bold white"
+            )
+ 
+        try:
+            riskiq_user = os.getenv("RISKIQ_USER")
+            riskiq_apikey = os.getenv("RISKIQ_KEY")
+            auth = (riskiq_user, riskiq_apikey)
+            data = {"query": self.target_domain}
+            LOG.debug("RiskIQ API GET request for %s", self.target_domain)
 
-        infratrack_table.add_column("IP", style='dim')
-        infratrack_table.add_column("Domain Name", style='dim')
-        infratrack_table.add_column("First Seen", style='dim')
-        infratrack_table.add_column("Last Seen", style='dim')
-        infratrack_table.add_column("Registrar", style='dim')
-        infratrack_table.add_column("VirusTotal Report", style='dim')
-        #infratrack_table.add_column("VirusTotal Tags", style='dim')
+            response = get(
+            "https://api.riskiq.net/pt/v2/dns/passive", auth=auth, params=data
+            )
+            riq_api_results = response.json()
 
-        infratrack_table.add_row(host_to_ip, domain_info, "2022-01-01", "2022-01-02", domain_info.registrar, self.virustotal_api_req_domaininfo(self.target_domain))
+            LOG.debug("Received a response: %s", riq_api_results)
+            for items in riq_api_results["results"]:
+                pdns_resolutions = items["resolve"]
+                first_seen = riq_api_results["firstSeen"]
+                last_seen = riq_api_results["lastSeen"]
 
-        
-        console.print(infratrack_table)
-        #console.print(
-            #f"[!] Registrar: {domain_info.registrar} \n[!] Name Servers; {domain_info.name_servers}",
-            #style="bold green"
-        #)
-        
-       
+            domain_info = whois.whois(self.target_domain)
+            infratrack_table = table.Table(
+            title="Domain Summary",
+            show_header=True,
+            header_style="white",
+            show_footer=False,
+            )
 
-        time.sleep(1)
-        #self.virustotal_api_req_domaininfo(self.target_domain)
-        time.sleep(1)
-        #self.virustotal_api_req_commfiles(self.target_domain)
-        time.sleep(1)
-        #self.virustotal_api_req_certinfo(self.target_domain)
-        print()
-        time.sleep(1)
-        # get first_seen and last_seen information from RiskIQ API
-        #riskiq_ip_resolutions(self.target_domain)
+            infratrack_table.add_column("IP", style="dim")
+            infratrack_table.add_column("Domain Name", style="dim")
+            infratrack_table.add_column("First Seen", style="dim")
+            infratrack_table.add_column("Last Seen", style="dim")
+            infratrack_table.add_column("Registrar", style="dim")
+            infratrack_table.add_column("VirusTotal Report", style="dim")
+            infratrack_table.add_column("VirusTotal Communicating Files", style='dim')
+
+            infratrack_table.add_row(
+            str(pdns_resolutions),
+            self.target_domain,
+            str(first_seen),
+            str(last_seen),
+            domain_info.registrar,
+            self.virustotal_api_req_domaininfo(self.target_domain),
+            self.virustotal_api_req_commfiles(self.target_domain)
+            )
+
+            console.print(infratrack_table)
+
+        except whois.parser.PywhoisError as err:
+            print(err)
