f058c5360cd8a1e760a421683834fb198e1ada84
Mike R <cr8syoki@gmail.com>
2022-12-28 20:28:36 +0900

    update ip_lkup_summary.py

--- infratrack/ip_lkup_summary.py
+++ infratrack/ip_lkup_summary.py
@@ -1,47 +1,70 @@
+"""_summary_ -- ip_lkup_summary.py"""
 import os
-import time
-from dotenv import load_dotenv
-import whois
 import sys
 import ipaddress
-from httpx import get
+from rich import table
 from rich.console import Console
+from dotenv import load_dotenv
+import whois
+from httpx import get
+import httpx
 from core.logs import LOG
-from riskiq_api_lookup import riskiq_ip_resolutions
+
 
 load_dotenv()
 console = Console()
 
-LOG.info("Starting...")
+VIRUSTOTAL_API= os.getenv("VT_API_KEY")
+GREYNOISE_API = os.getenv("GREYNOISE_API")
 
 class IPSummary:
     """_summary_ - IP address summary"""
+
     def __init__(self, target_ip: str):
         self.target_ip = target_ip
-        self.greynoiseapi = "https://api.greynoise.io/v3/community/{target_ip}"
-        self.greynoise_api_key = os.getenv("GREYNOISE_API")
+        self.greynoise_api_url = f"https://api.greynoise.io/v3/community/{target_ip}"
         self.vt_url = f"https://www.virustotal.com/api/v3/ip_addresses/{target_ip}"
         self.vt_comm_files = f"https://www.virustotal.com/api/v3/ip_addresses/{target_ip}/communicating_files"
-        self.vt_certs = f"https://www.virustotal.com/api/v3/ip_addresses/{target_ip}/historical_ssl_certificates"
-        self.vt_headers = os.getenv("VT_API_KEY")
-       
+        self.vt_ip_resolutions = (
+            f"https://www.virustotal.com/api/v3/ip_addresses/{target_ip}/resolutions"
+        )
+        self.gn_result = {}
 
-    def get_greynoise_data(self, target_ip: str) :
-        """_summary_ - GreyNoise API request"""
+    def get_greynoise_api(self, target_ip: str):
+        """_summary_ - GreyNoise API request
+        Args:
+        target_ip (str): The IP address you want to investigate.
+        """
         LOG.debug("GreyNoise GET request for %s", target_ip)
-        headers = {"key": self.greynoise_api_key}
-        response = get(self.greynoiseapi, headers=headers)
-        #console.print(f"[*] GreyNoise results for {target_ip}: ", style="bold white")
-        self.result = response.json()
-        LOG.debug("Received a response: %s", self.result)
-        console.print(f'GreyNoise classification: [bold red]{self.result.get("classification")} ', style=" white")
+        headers = {"key": GREYNOISE_API}
+        try:
+            response = get(self.greynoise_api_url, headers=headers)
+        except (httpx.HTTPError, httpx.ConnectTimeout):
+            self.api_error_logging_crit(
+                "Could not connect. Check the URL for your API."
+            )
 
-    def virustotal_api_req(self, target_ip: str):
-        #console.print(f"[*] VirusTotal results for {target_ip}: ", style="bold white")
-        headers = {"x-apikey": self.vt_headers}
+        self.gn_result = response.json()
+        LOG.debug("Received a response: %s", self.gn_result)
+
+    def get_vt_api_ip_info(self, target_ip: str):
+        """_summary_
+        Args:
+        target_ip (str): The IP address you want to investigate.
+        """
+        headers = {"x-apikey": VIRUSTOTAL_API}
 
         LOG.debug("VT API GET request for %s", self.vt_url)
-        response = get(self.vt_url, headers=headers)
+
+        try:
+
+            response = get(self.vt_url, headers=headers)
+
+        except (httpx.HTTPError, httpx.ConnectTimeout):
+            self.api_error_logging_crit(
+                "Could not connect. Check the URL for your API."
+            )
+
         results = response.json()
 
         LOG.debug("Received a response: %s", results)
@@ -51,96 +74,113 @@ class IPSummary:
         mal = last_analysis["malicious"]
 
         if mal == 0:
-            console.print(f"{target_ip} is clean", style="bold red")
-        else:
-            console.print(
-                f"[blue]{target_ip} [white]was identified as malicious by [red]{str(mal)} vendors",
-                style="bold green",
-            )
+            return f"{target_ip} is clean"
 
-    def virustotal_api_comm_files(self, target_ip: str):
-        LOG.debug("VT API GET request for %s", self.vt_comm_files)
-        headers = {"x-apikey": self.vt_headers}
-        response = get(self.vt_comm_files, headers=headers)
-        results = response.json()
-        LOG.debug("Received a response: %s", results)
-        vt_data = results["data"]
-        metadata = results["meta"]
-        file_count = metadata["count"]
+        return (
+            f"{target_ip} [white]was identified as malicious by [red]{str(mal)} vendors"
+        )
 
-        self.file_names = [
-            x["attributes"]["magic"]
-            for x in vt_data
-            if x["attributes"]["magic"] is not None
-        ]
+    def get_vt_api_ip_resolutions(self, target_ip: str):
+        """_summary_
+        Args:
+        target_ip (str): The IP address you want to investigate.
+        """
+        LOG.debug("VT API GET request for %s", self.vt_ip_resolutions)
+        headers = {"x-apikey": VIRUSTOTAL_API}
 
-        if file_count == 0:
-            console.print(
-                f"No communicating files", style="bold red"
-            )
-        else:
-            console.print(
-                f"[blue]{file_count} communicating files identified", style=" white"
-            )
+        LOG.debug("VT API GET request for %s", self.vt_ip_resolutions)
 
-    def virustotal_api_cert_info(self, target_ip: str):
-        LOG.debug("VT API GET request for %s", self.vt_certs)
-        headers = {"x-apikey": self.vt_headers}
-
-        LOG.debug("VT API GET request for %s", self.vt_certs)
-        response = get(self.vt_certs, headers=headers)
-        results = response.json()
-        LOG.debug("Received a response: %s", results)
-        vt_cert_data = list(results["data"])
-        metadata = results["meta"]
-        cert_count = metadata["count"]
-        
-        self.ca_issuer = [
-            x["attributes"]["issuer"]["O"]
-            for x in vt_cert_data
-            if x["attributes"]["issuer"]["O"] is not None
-        ]
-    
-        if cert_count == 0:
-            console.print("No historical SSL certificates found", style="bold white")
-        else:
-            console.print(
-                f"[white]{cert_count} historical SSL certificates for [green]{target_ip}",
-                style="green",
-            )
-            console.print(f'Certificate Issuer: {self.ca_issuer}', style="bold white")
-
- 
-       
-    def run(self):
-       
         try:
-           
-            self._process_info()
-        except ValueError as err:
+            response = get(self.vt_ip_resolutions, headers=headers)
+
+        except (httpx.HTTPError, httpx.ConnectTimeout):
+            self.api_error_logging_crit(
+                "Could not connect. Check the URL for your API."
+            )
+
+        vt_results = response.json()
+        LOG.debug("Received a response: %s", vt_results)
+        vt_data = vt_results["data"]
+
+        hist_resolutions = [
+            x["attributes"]["host_name"]
+            for x in vt_data
+            if x["attributes"]["host_name"] is not None
+        ]
+
+        return (
+            hist_resolutions
+            if isinstance(hist_resolutions, list)
+            else f"No historical domain resolutions for {target_ip} found."
+        )
+
+    def run(self):
+        """_summary_ -- Run the program."""
+        LOG.info("Starting ip_lkup_summary.py")
+        try:
+            self.build_table_from_output()
+        except ipaddress.AddressValueError as err:
             console.print(err, style="bold red")
             console.print("Invalid IP address", style="bold red")
             sys.exit(1)
 
-   
-    def _process_info(self):
-        ipaddress.ip_address(self.target_ip)
-        ip_info = whois.whois(self.target_ip)
-        console.print("         IP Address Summary", style="bold white")
-        console.print("         ---------------------", style="bold white")
-        console.print(f"IP: {self.target_ip} ", style="bold white")
-        console.print(
-            f"Registrar: [green] {ip_info.registrar}\n[white]Country: [green] {ip_info.country}",
+    def api_error_logging_crit(self, arg0):
+        """Standard logging message for errors/exceptions in API use."""
+        LOG.critical("Error in API URL, check it again.")
+        print(arg0)
+        sys.exit(1)
+
+    def build_table_from_output(self):
+        """_summary_ -- Combine all API output and build a table"""
+
+        riskiq_user = os.getenv("RISKIQ_USER")
+        riskiq_apikey = os.getenv("RISKIQ_KEY")
+        auth = (riskiq_user, riskiq_apikey)
+        data = {"query": self.target_ip}
+
+        LOG.debug("RiskIQ API GET request for %s", self.target_ip)
+        response = get(
+            "https://api.riskiq.net/pt/v2/dns/passive", auth=auth, params=data
         )
-        time.sleep(1)
+        riq_api_results = response.json()
 
-        self.get_greynoise_data(self.target_ip)
-        time.sleep(1)
-        self.virustotal_api_req(self.target_ip)
-        time.sleep(1)
-        self.virustotal_api_comm_files(self.target_ip)
-        time.sleep(1)
-        self.virustotal_api_cert_info(self.target_ip)
-        time.sleep(1)
-        riskiq_ip_resolutions(self.target_ip)
+        LOG.debug("Received a response: %s", riq_api_results)
+        for _ in riq_api_results["results"]:
+            #pdns_resolutions = items["resolve"]
+            first_seen = riq_api_results["firstSeen"]
+            last_seen = riq_api_results["lastSeen"]
 
+        ip_whois_info = whois.whois(self.target_ip)
+
+        ip_summ_table = table.Table(
+            title="IP Summary",
+            show_header=True,
+            header_style="white",
+            show_footer=False,
+        )
+
+        ip_summ_table.add_column("IP", style="cyan")
+        ip_summ_table.add_column("Registrar", style="magenta")
+        ip_summ_table.add_column("Greynoise Report", style="red")
+        ip_summ_table.add_column("VirusTotal Classification", justify="center", style="red")
+        ip_summ_table.add_column(
+            "VirusTotal Historical Resolutions", justify="center", style="red"
+        )
+        ip_summ_table.add_column("First Seen", justify="right", style="green")
+        ip_summ_table.add_column("Last Seen", justify="right", style="green")
+        
+        try:
+            ip_summ_table.add_row(
+                self.target_ip,
+                ip_whois_info.registrar,
+                str(self.gn_result.get("classification")),
+                self.get_vt_api_ip_info(self.target_ip),
+                str(self.get_vt_api_ip_resolutions(self.target_ip)),
+                str(first_seen),
+                str(last_seen),
+            )
+        except Exception:
+            console.print_exception(show_locals=True)
+
+        console.print(ip_summ_table)
+        print()
